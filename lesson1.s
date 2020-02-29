934a21d0cb44925cab67c92f4787a1a8cb4cb28f
Luis F Caro <luisfcaro@pop-os.localdomain>
2020-02-28 21:27:15 -0400

    adding

--- lesson1.s
+++ lesson1.s
@@ -7,7 +7,7 @@
    lw $t0, 32($s3)
 
 #storing words
-   sw $t0, 48(#s3)
+   sw $t0, 48($s3) #save $t0 into the offset 48 of register $s3 
 
   #adding register with a constant
     addi $s3, $s3, 4
@@ -51,3 +51,49 @@
           bne $t0, $s5, Exit #if the value of i != k then go to Branch Exit
           addi $s3, $s3, 1 #else increment i by one
           j Loop  #restart the loop
+      Exit: #Do nothing
+
+    #More conditionals
+      slt rd, rs, rt # if( rs < rt ) rd = 1, else rt = 0
+      slti rt, rs, constant # if( rs < constant) rt=1, else rt =
+
+    #Combination with beq
+      slt $t0, $s1, $s2 #if( $s1 < $s2) $t0 = 1, else $t0 = 0
+      bne $t0, $zero, L # go to branch L
+
+      #Signed comparisons
+        slt, slti
+
+      #Unsigned Comparisons
+        sltu, sltui
+
+      #Register Usage
+        #Arguments
+          $a0 - $a3 #reg's 4-7
+
+        #Result Values
+          $v0, $v1 #reg's 2 & 3
+
+        #Temporaries (Can be overwritten)
+          $t0 - $t9
+
+        #Saved
+          $s0 - $s7 #Must be saved and restored by callee
+
+        #Global pointer
+          $gp #static data reg28
+
+        #Stack pointer
+          $sp #reg29
+
+        #Frame pointer
+          $fp #reg30
+
+        #Return Address
+          $ra #reg31
+
+        #Procedure Call: Jump and Link
+          jal ProcedureLabel #jumps to a label and store address in $ra
+
+        #Procedure Return: Jump register
+          jr $ra #returns to an address in a register (return)
